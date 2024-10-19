51ba574a0ae889f26b3af12e2c84ca51df2df283
Joakim <159180076+FunkyBeatz@users.noreply.github.com>
2024-10-20 00:41:54 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,5 @@
 # Flask setup to keep the bot alive
+import os
 from flask import Flask
 from threading import Thread
 
@@ -9,10 +10,13 @@ def home():
     return "I'm alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Use the PORT environment variable assigned by Render
+    port = int(os.environ.get('PORT', 8080))  # Default to 8080 if no port is assigned
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
 
 # Using this 2nd folder only to make it work in Vercel
