8f0b8816c6ad67c53d3c4282f031371c2d21375d
Joakim <funkybeatzeth@gmail.com>
2024-10-19 20:34:03 +0200

    Add keep-alive Flask server

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+# Flask setup to keep the bot alive
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# Using this 2nd folder only to make it work in Vercel
